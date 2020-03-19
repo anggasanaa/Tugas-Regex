af383659ff165b9dc6b3f274951023d544fd8ad7
angghero <martuaanggasana@gmail.com>
2020-03-19 12:41:46 +0700

    Introduction Java

---
+++ src/latihanaregex/LatihanaRegex.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanaregex;
+
+/**
+ *
+ * @author hello
+ */
+public class LatihanaRegex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame frame = new MainFrame();
+        frame.show();
+        
+        frame.setTitle("2SC4 Third Program Task Regex - [Martua Anggasana] [1920010123]");
+    }
+    
+}
